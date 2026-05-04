4cbacbdc094bb408c73c6be4d5888465a6d801ef
Flowikus <flovikirill@gmail.com>
2026-05-04 15:41:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,10 @@
 const myImage = document.querySelector("img");
 myImage.addEventListener("click", () => {
     const mySrc = myImage.getAttribute("src");
-    if (mySrc === "images/cpu.png") {
-        myImage.setAttribute("src", "images/cpu2.jpg");
+    if (mySrc === "cpu.png") {
+        myImage.setAttribute("src", "cpu2.jpg");
     } else {
-        myImage.setAttribute("src", "images/cpu.png")
+        myImage.setAttribute("src", "cpu.png")
     }
 });
 
